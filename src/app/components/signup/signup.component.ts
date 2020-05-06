fbf4c3e0d6d057887594626158ad25ad40f31230
moominzie <yammybbraimong@gmail.com>
2020-05-07 04:02:55 +0700

    lab4

---
+++ src/app/components/signup/signup.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit} from '@angular/core';
+import { FormControl, FormGroup, FormArray, Validators } from '@angular/forms';
+import { from } from 'rxjs';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+
+export class SignupComponent implements OnInit {
+
+  profileForm = new FormGroup({
+    title: new FormControl(''),
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    sex: new FormControl(''),
+    email: new FormControl('',[Validators.required, Validators.email]),
+    stdid: new FormControl('',[Validators.required,Validators.pattern('B[0-9]{7}')]),
+    address: new FormGroup({
+      street: new FormControl(''),
+      city: new FormControl(''),
+      state: new FormControl(''),
+      zip: new FormControl(''),
+    }),
+    aliases: new FormArray([
+      new FormControl('')
+    ])
+  })
+
+  get email(){ return this.profileForm.get('email'); }
+
+  get stdid(){ return this.profileForm.get('stdid'); }
+
+  get aliases(){
+    return this.profileForm.get('aliases') as FormArray;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  addAlias(){
+    this.aliases.push(new FormControl(''));
+  }
+
+}
